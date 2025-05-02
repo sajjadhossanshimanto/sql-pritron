4b2e5f5af1035468f66050f73b5317947b9f119d
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-05-02 12:02:32 +0600

    and or condition in the selection

---
+++ 7. logical, and or.sql
@@ -0,0 +1,5 @@
+-- show details of them who are greter than 30 years old and scalary >12k
+select * from employee where age<30 and scalary>12000;
+-- just show the name of them
+select name from employee where age<30 and scalary>12000;
+
