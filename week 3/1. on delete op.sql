7d404e51a6b6d10756197d61b77912e75cf4c416
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-05-16 12:30:01 +0600

    basic table structure with foreign key

---
+++ week 3/1. on delete op.sql
@@ -0,0 +1,24 @@
+use programminghero;
+
+
+create table student(
+	roll char(4) primary key,
+    name varchar(50) not null,
+    email varchar(50) unique,
+    address varchar(120),
+    age int check(age>10)
+);
+
+create table Course(
+	CourseNo varchar(5) primary key,
+    CourseName varchar(50)
+);
+
+create table Email(
+	Roll varchar(5),
+    CourseNo varchar(5),
+    enroll_date date,
+    foreign key(roll) references Student(roll),
+    foreign key(CourseNo) references Course(CourseNo)
+);
+
